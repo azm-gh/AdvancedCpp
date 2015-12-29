7ba6a32bbc3e5d4110d90eb2a8eca686e0998542
azm-gh <ableton2@centrum.cz>
2015-12-29 18:34:30 +0100

    Create Virtual Destructor and Smart Destructor.cpp

---
+++ Virtual Destructor and Smart Destructor.cpp
@@ -0,0 +1,62 @@
+
+/*Using virtual destructors in a polymorphic base classes
+https://www.youtube.com/watch?v=ZiNGWHg5Z-o&list=PLE28375D4AC946CC3&index=6
+https://stackoverflow.com/questions/5873515/why-should-the-destructor-of-base-classes-be-virtual
+
+
+If a class is to be used in polymoprhic way. Such as in this case where we create yellow dog
+and then cast it into Dog* pointer, then the base class has to have virtual destructor to insure
+the destructors are invoked appropriately.
+
+If a class has some virtual function, it is most likely it will be used polymorphically which means
+it will need virtual destructor
+
+If you dont want to use virtual destructor you could use shared_pointer
+
+Note: All classes in STL have no virtual destructor, so be careful inheriting from them. Use shared pointer
+If you are deriving from STL classes
+
+*/
+
+class Dog
+{
+public:
+	virtual ~Dog()  // without keyword virtual, only dog is destroyed
+	{
+		std::cout << "Dog destoyed" << std::endl;
+	}
+	virtual void bark();
+};
+
+class YellowDog : public Dog
+{
+public:
+	~YellowDog()
+	{
+		std::cout << "Yellow dog is destroyed." << std::endl;
+	}
+};
+
+class DogFactory
+{
+public:
+	static Dog* createYellowDog()
+	{
+		return(new YellowDog());
+
+	}
+	//Create other dogs....
+};
+
+
+int main()
+{
+	Dog* pd = DogFactory::createYellowDog();
+	//do something with pd
+
+	delete pd;
+
+
+	std::cout << std::endl;
+	system("pause");
+	return 0;
